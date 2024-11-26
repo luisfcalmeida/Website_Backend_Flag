bf543adce37a323e8296332b5553b8ee64cb3bb7
Luís de Almeida <luisfca@outlook.pt>
2024-11-26 18:09:59 +0000

    Implementação da listagem de todos os veículos, página de detalhe do veículo e formulário para criação de novo veículo

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [CarController::class, 'index'])
+    ->name('index');
+
+Route::get('/details', [CarController::class, 'details'])
+    ->name('details');
+
+Route::get('/create', [CarController::class, 'create'])
+    ->name('create');
+
+Route::post('/store', [CarController::class, 'store'])
+    ->name('store');
+
+Route::get('/{id}', [CarController::class, 'details'])
+    ->name('details')
+    ->whereNumber('id');
